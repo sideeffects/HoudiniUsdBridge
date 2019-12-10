e1a4ebab1f1dea4437f9d2263ec53020d7a751e9
SideFX <sidefx@sidefx.com>
2019-12-10 01:00:18 -0500

    Matching to Houdini build 18.5.55.

--- src/houdini/lib/H_USD/HUSD/HUSD_Skeleton.h
+++ src/houdini/lib/H_USD/HUSD/HUSD_Skeleton.h
@@ -19,8 +19,12 @@
 
 #include "HUSD_API.h"
 
+#include <GU/GU_AgentRig.h>
 #include <SYS/SYS_Types.h>
 
+class GU_AgentClip;
+class GU_AgentLayer;
+class GU_AgentShapeLib;
 class GU_Detail;
 class HUSD_AutoReadLock;
 class UT_StringHolder;
@@ -55,5 +59,28 @@ HUSD_API bool
 HUSDimportSkeletonPose(GU_Detail &gdp, const HUSD_AutoReadLock &readlock,
                        const UT_StringRef &skelrootpath,
                        HUSD_SkeletonPoseType pose_type, fpreal time);
+
+/// Builds an agent rig from the SkelRoot's first Skeleton prim.
+HUSD_API GU_AgentRigPtr
+HUSDimportAgentRig(const HUSD_AutoReadLock &readlock,
+                   const UT_StringRef &skelrootpath,
+                   const UT_StringHolder &rig_name);
+
+/// Imports all skinnable primitives underneath the provided SkelRoot prim
+/// (which are associated with the skeleton used for HUSDimportRig()), and adds
+/// the shape bindings to the provided layer.
+HUSD_API bool
+HUSDimportAgentShapes(GU_AgentShapeLib &shapelib,
+                      GU_AgentLayer &layer,
+                      const HUSD_AutoReadLock &readlock,
+                      const UT_StringRef &skelrootpath,
+                      fpreal layer_bounds_scale);
+
+/// Initialize an agent clip from the animation associated with the skeleton
+/// used for HUSDimportAgentRig().
+HUSD_API bool
+HUSDimportAgentClip(GU_AgentClip &clip,
+                    HUSD_AutoReadLock &readlock,
+                    const UT_StringRef &skelrootpath);
 
 #endif
